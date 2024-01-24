baaedf041e458ecd709263a7204eeac6267792b5
carolrochafloro <carolinarochaf@hotmail.com>
2024-01-24 16:26:08 -0300

    Adicionada função para enviar token para a tabela de tokens válidos e mensagem de erro no console informando o controller.

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -2,37 +2,42 @@ import database from '../database/db.js'
 import User from '../models/user.js'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
+import Token from '../models/token.js'
 
-/* controller login: recebe no body email e senha, busca usuario no db, compara senha com bcrypt. Se não existe o usuário, mensagem genérica de usuário ou senha inválidos. Se existe usuário, gerar token. Desestruturação da variável user para excluir a senha e retornar os demais dados do usuário logado junto com o token. */
+/* controller login: recebe no body email e senha, busca usuario no db, compara senha com bcrypt. Se não existe o usuário, mensagem genérica de usuário ou senha inválidos. Se existe usuário, gerar token. Desestruturação da variável user para excluir a senha e retornar os demais dados do usuário logado junto com o token. O token é enviado para a tabela de tokens como válido. */
 
-const userLogin = {
-  login: async (req, res) => {
-    await database.sync()
-    const { email, password } = req.body
-    try {
-      const user = await User.findOne({
-        where: {
-          email,
-        },
-      })
-      if (!user) {
-        return res.status(404).json({ mensagem: 'Usuário não encontrado.' })
-      }
-
-      const senhaValida = await bcrypt.compare(password, user.password)
-      if (!senhaValida) {
-        return res.status().json({ mensagem: 'Usuári ou senha inválidos' })
-      }
-      const token = jwt.sign({ id: user.id }, process.env.JWT_PASSWORD, {
-        expiresIn: process.env.JWT_EXPIRE,
-      })
-      const { password: _, ...userLogado } = user
-      return res.status(200).json({ userLogado, token })
-    } catch (error) {
-      console.error(error)
-      res.status(500)
+const userLogin = async (req, res) => {
+  const { email, password } = req.body
+  try {
+    const user = await User.findOne({
+      where: {
+        email,
+      },
+    })
+    if (!user) {
+      return res.status(404).json({ mensagem: 'Usuário não encontrado.' })
     }
-  },
+
+    const senhaValida = await bcrypt.compare(password, user.password)
+    if (!senhaValida) {
+      return res.status(401).json({ mensagem: 'Usuári ou senha inválidos' })
+    }
+    const token = jwt.sign({ id: user.id }, process.env.JWT_PASSWORD, {
+      expiresIn: process.env.JWT_EXPIRE,
+    })
+
+    await Token.create({
+      token,
+      isValid: true,
+      user_id: user.id,
+    })
+
+    const { password: _, ...userLogado } = user
+    return res.status(200).json({ userLogado, token })
+  } catch (error) {
+    console.error(error, 'Erro no controller de login')
+    res.status(500).json({ mensagem: 'Erro intero do servidor.' })
+  }
 }
 
 export default userLogin
