5edb2a0f9e54e4791fcb706b04ed02262c49e5c4
carolrochafloro <carolinarochaf@hotmail.com>
2024-01-24 16:24:11 -0300

    Adicionado database.sync ao app.js com mensagem no console em caso de erro de sincronização. Utilizado force: false para prevenir a criação de novas tabelas ao reiniciar a aplicação, mantendo os dados já existentes.

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import { userRoutes, portfolioRoutes } from './src/routes/index.js'
+import database from './src/database/db.js'
 
 dotenv.config()
 
 const app = express()
 app.use(express.json())
+try {
+  await database.sync({ force: false })
+} catch (error) {
+  console.error('Erro ao sincronizar com o banco de dados.')
+}
+
 app.use(userRoutes)
 app.use(portfolioRoutes)
 
