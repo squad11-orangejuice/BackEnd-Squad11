d394c1a5cb890f03b65755b66ba34d7cc6d02978
Welen1911 <welenalmeida2210@gmail.com>
2024-01-24 11:08:01 -0300

    Criacao do campo google_id

--- src/models/user.js
+++ src/models/user.js
@@ -1,30 +1,36 @@
-const {DataTypes} = require("sequelize"); 
+const { DataTypes } = require("sequelize");
 
-const database = require('../database/db');
+const database = require("../database/db");
 
-const User = database.define('user', {
-    id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        allowsNull: false,
-        primaryKey: true
-    },
-    nome: {
-        type: DataTypes.STRING,
-        allowsNull: false,
-    },
-    sobrenome: {
-        type: DataTypes.STRING,
-        allowsNull: false
-    },
-    email: {
-        type: DataTypes.STRING,
-        allowsNull: false,
-        unique: true
-    },
-    password: {
-        type: DataTypes.STRING,
-    }
+const User = database.define("user", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    allowsNull: false,
+    primaryKey: true,
+  },
+  nome: {
+    type: DataTypes.STRING,
+    allowsNull: false,
+  },
+  sobrenome: {
+    type: DataTypes.STRING,
+    allowsNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowsNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowsNull: true,
+  },
+  google_id: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowsNull: true,
+  },
 });
 
 module.exports = User;
