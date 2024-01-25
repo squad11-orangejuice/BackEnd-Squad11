0be2dae8d051d51bc3460cdf989e638f6718c55d
Welen1911 <welenalmeida2210@gmail.com>
2024-01-25 11:05:32 -0300

    Resolvido routa de cadastrar usuario

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,18 +1,16 @@
-import express from 'express'
-import loginSocial from '../controllers/loginSocial.js'
-import userLogin from '../controllers/loginController.js'
-import UserController from '../controllers/UserController.js'
-const userRoutes = express()
+import express from "express";
+import loginSocial from "../controllers/loginSocial.js";
+import userLogin from "../controllers/loginController.js";
+import create from "../controllers/RegisterController.js";
+const userRoutes = express();
 
-userRoutes.post('/cadastrar', async (req, res) => {
-  await UserController.create(req, res)
-})
-userRoutes.post('/login', userLogin)
-userRoutes.post('/login/social', loginSocial)
-userRoutes.post('/login/out')
+userRoutes.post("/cadastrar", create);
+userRoutes.post("/login", userLogin);
+userRoutes.post("/login/social", loginSocial);
+userRoutes.post("/login/out");
 
 // midd autenticação a partir daqui
 // home recebe params p/ buscar tags
-userRoutes.get('/home')
+userRoutes.get("/home");
 
-export default userRoutes
+export default userRoutes;
