82ce6cc440926e7294d58c8b0749f5d8390f1b5d
Welen1911 <welenalmeida2210@gmail.com>
2024-01-25 10:55:37 -0300

    resolvendo conflitos do pr

--- src/database/db.js
+++ src/database/db.js
@@ -2,15 +2,15 @@ import Sequelize from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
 
+console.log(process.env.DB_PASSWORD);
 const sequelize = new Sequelize(
-  process.env.DB_NAME || "orange_portfolio",
-  process.env.DB_USER || "postgres",
-  process.env.DB_PASSWORD || "admin",
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
   {
     dialect: "postgres",
     host: process.env.DB_HOST || "localhost",
     port: process.env.DB_PORT || 5432,
   }
 );
-
 export default sequelize;
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,10 +1,15 @@
 import express from "express";
-
-import create from "../controllers/RegisterController.js";
+import loginSocial from "../controllers/loginSocial.js";
+import userLogin from "../controllers/loginController.js";
+import UserController from "../controllers/UserController.js";
 const userRoutes = express();
 
-userRoutes.post("/cadastrar", create);
-userRoutes.post("login");
+userRoutes.post("/cadastrar", async (req, res) => {
+  await UserController.create(req, res);
+});
+userRoutes.post("/login", userLogin);
+userRoutes.post("/login/social", loginSocial);
+userRoutes.post("/login/out");
 
 // midd autenticação a partir daqui
 // home recebe params p/ buscar tags
