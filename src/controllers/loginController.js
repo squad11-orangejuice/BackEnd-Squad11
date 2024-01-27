630d52f27262b14b5f11556679d89a8c396249ce
Welen1911 <welenalmeida2210@gmail.com>
2024-01-27 19:11:26 -0300

    Feito middleware de verificar o dono do projeto

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,46 +1,44 @@
-import User from '../models/user.js'
-import bcrypt from 'bcrypt'
-import jwt from 'jsonwebtoken'
-import Token from '../models/token.js'
-import database from '../database/db.js'
-import dotenv from 'dotenv'
-dotenv.config()
+import User from "../models/user.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import Token from "../models/token.js";
+import database from "../database/db.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 /* controller login: recebe no body email e senha, busca usuario no db, compara senha com bcrypt. Se não existe o usuário, mensagem genérica de usuário ou senha inválidos. Se existe usuário, gerar token. Desestruturação da variável user para excluir a senha e retornar os demais dados do usuário logado junto com o token. O token é enviado para a tabela de tokens como válido. */
 
 const userLogin = async (req, res) => {
-  await database.sync({ force: false })
-  const { email, password } = req.body
+  await database.sync({ force: false });
+  const { email, password } = req.body;
   try {
     const user = await User.findOne({
       where: {
         email,
       },
-    })
+    });
     if (!user) {
-      return res.status(404).json({ mensagem: 'Usuário não encontrado.' })
+      return res.status(404).json({ mensagem: "Usuário não encontrado." });
     }
 
-    const senhaValida = await bcrypt.compare(password, user.password)
+    const senhaValida = await bcrypt.compare(password, user.password);
     if (!senhaValida) {
-      return res.status(401).json({ mensagem: 'Usuári ou senha inválidos' })
+      return res.status(401).json({ mensagem: "Usuári ou senha inválidos" });
     }
-    const token = jwt.sign({ id: user.id }, process.env.JWT_PASSWORD, {
-      expiresIn: process.env.JWT_EXPIRE,
-    })
+    const token = jwt.sign({ id: user.id }, process.env.JWT_PASSWORD);
 
     await Token.create({
       token,
       isValid: true,
       user_id: user.id,
-    })
+    });
 
-    const { password: _, google_id: __, ...userLogado } = user
-    return res.status(200).json({ userLogado, token })
+    const { password: _, google_id: __, ...userLogado } = user;
+    return res.status(200).json({ userLogado, token });
   } catch (error) {
-    console.error(error, 'Erro no controller de login')
-    res.status(500).json({ mensagem: 'Erro intero do servidor.' })
+    console.error(error, "Erro no controller de login");
+    res.status(500).json({ mensagem: "Erro intero do servidor." });
   }
-}
+};
 
-export default userLogin
+export default userLogin;
--- src/routes/portfolioRoutes.js
+++ src/routes/portfolioRoutes.js
@@ -1,13 +1,19 @@
 import express from "express";
 import deleteProjeto from "../controllers/deleteProjetoController.js";
+import middlewareAutenticacao from "../middleware/middlewareAutenticacao.js";
+import middlewareUserProjeto from "../middleware/middlewareUserProjeto.js";
 
 const portfolioRoutes = express();
+
+portfolioRoutes.use(middlewareAutenticacao);
 
 portfolioRoutes.get("/portfolio");
 portfolioRoutes.post("/projeto/novo");
 // midd verificar se projetos pertencem ao usuário logado
-portfolioRoutes.put("/projeto/editar/:id");
-portfolioRoutes.delete("/projeto/deletar/:id", deleteProjeto);
+portfolioRoutes.use("/projeto/:id", middlewareUserProjeto);
+
+portfolioRoutes.put("/projeto/:id");
+portfolioRoutes.delete("/projeto/:id", deleteProjeto);
 portfolioRoutes.get("/projeto/:id");
 
 export default portfolioRoutes;
