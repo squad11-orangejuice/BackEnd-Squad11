284a44e915843884757351afee416fdeb72b5554
carolrochafloro <carolinarochaf@hotmail.com>
2024-01-27 19:19:09 -0300

    Configuração inicial do controller de novo projeto - em andamento

--- src/controllers/novoProjeto.js
+++ src/controllers/novoProjeto.js
@@ -1,36 +1,42 @@
 import database from '../database/db.js'
+import User from '../models/user.js'
 import Projeto from '../models/projeto.js'
 import upload from '../utils/multerConfig.js'
 import dotenv from 'dotenv'
 // import Projeto_tag from '../models/projeto_tag.js'
-import Tag from '../models/tag.js'
+// import Tag from '../models/tag.js'
 dotenv.config()
 /* Recebe multipart form data, importa o multer do arquivo de configurações como upload, envia imagem para bucket, salva string do endereço. Dados do projeto vão pra table projetos, tags vão para projeto_tags. */
 const novoProjeto = async (req, res) => {
+  await database.sync()
   try {
-    upload.single('arquivo')(req, res, async (err) => {
+    upload.single('imagem')(req, res, async (err) => {
       if (err) {
-        console.error('Erro ao processar o arquivo', err)
+        console.error('Erro no upload', err)
         return res.status(500).send('Erro interno do servidor')
       }
-    })
-    const linkArquivo = req.file.path
-    console.log(linkArquivo)
-    const { titulo, link, descricao, tags } = req.body
-    await Projeto.create({
-      titulo,
-      link,
-      descricao,
-      imagem: linkArquivo,
-    })
+      const { titulo, link, descricao, tags } = req.body
+      const linkArquivo = req.file.path
+      console.log(linkArquivo)
+      await Projeto.create({
+        titulo,
+        link,
+        descricao,
+        imagem: linkArquivo,
+        data: new Date(),
+        user_id: req.user.id,
+      })
 
-    const arrayTags = tags.split(' ')
-    console.log(arrayTags)
-    const tagsCriadas = await Promise.all(
-      arrayTags.map((nome) => Tag.findOrCreate({ where: { nome } })),
-    )
+      // const arrayTags = tags.split(' ')
+      // console.log(arrayTags)
+      // const tagsCriadas = await Promise.all(
+      //   arrayTags.map((nome) => Tag.findOrCreate({ where: { nome } })),
+      // )
 
-    console.log(tagsCriadas)
+      // console.log(tagsCriadas)
+
+      return res.status(201).send('Projeto criado.')
+    })
   } catch (error) {
     console.error('Erro em novoProjeto', error)
     return res.status(500).send('Erro interno do servidor')
