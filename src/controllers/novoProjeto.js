9f4e86c0ea49ec822654910b344d12b94a13a169
carolrochafloro <carolinarochaf@hotmail.com>
2024-01-28 08:28:13 -0300

    Controller novo projeto - funcionando

--- src/controllers/novoProjeto.js
+++ src/controllers/novoProjeto.js
@@ -2,35 +2,64 @@ import database from '../database/db.js'
 import Projeto from '../models/projeto.js'
 import upload from '../utils/multerConfig.js'
 import dotenv from 'dotenv'
-// import Projeto_tag from '../models/projeto_tag.js'
+import Projeto_tag from '../models/projeto_tag.js'
 import Tag from '../models/tag.js'
 dotenv.config()
-/* Recebe multipart form data, importa o multer do arquivo de configurações como upload, envia imagem para bucket, salva string do endereço. Dados do projeto vão pra table projetos, tags vão para projeto_tags. */
+/* Recebe multipart form, importa o multer do arquivo de configurações como upload, envia imagem para bucket, salva string do endereço. Dados do projeto vão pra table projetos, utilizo split para separar as tags. Novas tags são salvas na tabela tags, todos os ids das tags (novas e antigas) são salvos em um array. Utilizado o método map para obter os ids das tags e depois envira para a tabela projeto_tags. */
 const novoProjeto = async (req, res) => {
+  await database.sync()
   try {
-    upload.single('arquivo')(req, res, async (err) => {
+    upload.single('imagem')(req, res, async (err) => {
       if (err) {
-        console.error('Erro ao processar o arquivo', err)
+        console.error('Erro no upload', err)
         return res.status(500).send('Erro interno do servidor')
       }
-    })
-    const linkArquivo = req.file.path
-    console.log(linkArquivo)
-    const { titulo, link, descricao, tags } = req.body
-    await Projeto.create({
-      titulo,
-      link,
-      descricao,
-      imagem: linkArquivo,
-    })
 
-    const arrayTags = tags.split(' ')
-    console.log(arrayTags)
-    const tagsCriadas = await Promise.all(
-      arrayTags.map((nome) => Tag.findOrCreate({ where: { nome } })),
-    )
+      const { titulo, link, descricao, tags } = req.body
 
-    console.log(tagsCriadas)
+      // Criar projeto e salvar o retorno em uma variável para obter id (enviado para projeto_tags)
+      const linkArquivo = req.file.path
+      const projetoCriado = await Projeto.create({
+        titulo,
+        link,
+        descricao,
+        imagem: linkArquivo,
+        data: new Date(),
+        user_id: req.user.id,
+      })
+
+      // Separar tags recebidas via multipart form, verificar se já existe e criar caso não existam. Salvar em uma variável. Obter tag_id.
+      const arrayTags = tags.split(',')
+      const projetoId = projetoCriado.id
+
+      const tagsCriadas = await Promise.all(
+        arrayTags.map(async (nome) => {
+          try {
+            const [tag, created] = await Tag.findOrCreate({ where: { nome } })
+            const tagId = tag.id
+            return tagId
+          } catch (error) {
+            console.error(`Erro ao criar/encontrar a tag ${nome}:`, error)
+            return null
+          }
+        }),
+      )
+
+      await Promise.all(
+        tagsCriadas.map(async (id) => {
+          try {
+            await Projeto_tag.create({
+              projeto_id: projetoId,
+              tag_id: id,
+            })
+          } catch (error) {
+            return res.status(500).send('Erro interno do servidor')
+          }
+        }),
+      )
+
+      return res.status(201).send('Projeto criado.')
+    })
   } catch (error) {
     console.error('Erro em novoProjeto', error)
     return res.status(500).send('Erro interno do servidor')
