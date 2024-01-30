eb676110d8a9c2eb6458ed1d92ee158c9c37bfb5
carolrochafloro <carolinarochaf@hotmail.com>
2024-01-30 13:09:05 -0300

    Correção de erro na rota de login social

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -7,9 +7,9 @@ import descobrirProjetos from '../controllers/descobrir.js'
 import middlewareAutenticacao from '../middleware/middlewareAutenticacao.js'
 const userRoutes = express()
 
+userRoutes.post('/usuario/login/google', loginSocial)
 userRoutes.post('/usuario/cadastrar', create)
 userRoutes.post('/usuario/login', userLogin)
-userRoutes.post('usuario/login/google', loginSocial)
 userRoutes.post('/usuario/logout', logout)
 
 userRoutes.get('/descobrir', middlewareAutenticacao, descobrirProjetos)
