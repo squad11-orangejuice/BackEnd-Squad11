0e468b2582c2d7f94286c0a6ac7237b5db896768
carolrochafloro <carolinarochaf@hotmail.com>
2024-01-30 21:14:57 -0300

    Ajuste rota novo projeto

--- src/routes/portfolioRoutes.js
+++ src/routes/portfolioRoutes.js
@@ -4,13 +4,14 @@ import middlewareAutenticacao from '../middleware/middlewareAutenticacao.js'
 import middlewareUserProjeto from '../middleware/middlewareUserProjeto.js'
 import editarProjeto from '../controllers/editarProjeto.js'
 import verPortfolio from '../controllers/verPortfolio.js'
+import novoProjeto from '../controllers/novoProjeto.js'
 
 const portfolioRoutes = express()
 
 portfolioRoutes.use(middlewareAutenticacao)
 
 portfolioRoutes.get('/portfolio', verPortfolio)
-portfolioRoutes.post('/projeto/novo')
+portfolioRoutes.post('/projeto/novo', novoProjeto)
 // midd verificar se projetos pertencem ao usuário logado
 portfolioRoutes.use('/projeto/:id', middlewareUserProjeto)
 
