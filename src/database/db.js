2354c41f349f76d73a66a20eaae55fb28c76ba84
Welen1911 <welenalmeida2210@gmail.com>
2024-01-31 14:20:21 -0300

    Modificado configuracao do db.js

--- src/database/db.js
+++ src/database/db.js
@@ -1,15 +1,20 @@
-import Sequelize from 'sequelize'
-import dotenv from 'dotenv'
-dotenv.config()
+import Sequelize from "sequelize";
+import dotenv from "dotenv";
+dotenv.config();
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    dialect: 'postgres',
-    host: process.env.DB_HOST || 'localhost',
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+    host: process.env.DB_HOST || "localhost",
     port: process.env.DB_PORT || 5432,
-  },
-)
-export default sequelize
+  }
+);
+export default sequelize;
